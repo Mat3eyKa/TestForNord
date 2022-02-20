ee1ffc01cf079b89781eb4daae9e621ad51cb8f3
Матвей Лемешев <matbeyka004@gmail.com>
2022-02-20 19:02:20 +0300

    last

--- TestForNord/Program.cs
+++ TestForNord/Program.cs
@@ -2,63 +2,68 @@
 
 namespace TestForNord
 {
-  //  1
-  //  2 3
-  //  4 5 6
- 
     class Program
     {
         static void Main(string[] args)
         {
-            int couple = 0;
-            int [][] TriangleArray= new int [6][];
-            TriangleArray[0] = new int[1];
-            TriangleArray[1] = new int[2];
-            TriangleArray[2] = new int[3];
-            TriangleArray[3] = new int[4];
-            TriangleArray[4] = new int[5];
-            TriangleArray[5] = new int[6];
-            Random array = new Random();
-            for (int i = 0;  i < TriangleArray.Length; i++)
-            {
-                for (int j = 0; j < TriangleArray[i].Length; j++)
-                {
-                    TriangleArray[i][j] = array.Next(10);
-                }
-
-            }
-            // Array.Reverse(TriangleArray);
-            for (int i = 0; i < TriangleArray.Length; i++)
-            {
-                for (int j = 0; j < TriangleArray[i].Length; j++)
-                {
-                    Console.Write(TriangleArray[i][j] + "\t");
-                }
-                Console.WriteLine();
-
-            }
-            int[] CoupleArray = new int[3];
-            for (int i = TriangleArray.Length-2; i >=0 ; i--)
-            {
-                for (int j = 0; j < TriangleArray[i].Length; j++)
-                {
-                    if (TriangleArray[i + 1][j] > TriangleArray[i + 1][j + 1])
-                        TriangleArray[i][j] += TriangleArray[i + 1][j];
-                    else
-                        TriangleArray[i][j] += TriangleArray[i + 1][j + 1];
-                }
-
-            }
-            for (int i = 0; i < TriangleArray.Length; i++)
-            {
-                for (int j = 0; j < TriangleArray[i].Length; j++)
-                {
-                    Console.Write(TriangleArray[i][j] + "\t");
-                }
-                Console.WriteLine();
-
-            }
+            int[][] TriangleArray = Init(6);
+            Console.Write("Изначальный массив\n");
+            Print(TriangleArray);
+            int[][] resultArray = Result(TriangleArray);
+            Console.Write("Максмимальная сумма из всех возможных маршрутов:" + resultArray[0][0]+ "\n");
+            Console.Write("Результирующий массив\n");
+            Print(resultArray);
 
         }
+        private static int[][] Init(int line)
+        {
+            int[][] TriangleArray = new int[line][];
+            for (int i = 0; i < line; i++)
+            {
+                TriangleArray[i] = new int[i + 1];
+            }
+            Random count = new Random();
+            for (int i = 0; i < TriangleArray.Length; i++)
+            {
+                for (int j = 0; j < TriangleArray[i].Length; j++)
+                {
+                    TriangleArray[i][j] = count.Next(100);
+                }
+            }
+            return TriangleArray;
+        }
+        private static void Print(int[][] array)
+        {
+            //вывод значений массива
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    Console.Write(array[i][j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        private static int[][] Result(int[][] array)
+        {
+            // вычисляем максимальное значение маршрута занося всю сумму в [0][0]
+            for (int i = array.Length - 2; i >= 0; i--)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    if (array[i + 1][j] > array[i + 1][j + 1])
+                    {
+                        array[i][j] += array[i + 1][j];
+
+                    }
+                    else
+                    {
+                        array[i][j] += array[i + 1][j + 1];
+                    }
+                }
+            }
+            return array;
+        }
     }
+
 }
